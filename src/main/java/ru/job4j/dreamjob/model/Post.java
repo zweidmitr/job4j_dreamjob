69815e704caeb8b89f276bc981f7e40bebc48626
Dmitrii Zwei <zweidmitr@gmail.com>
2022-05-04 17:48:10 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -1,10 +1,12 @@
 package ru.job4j.dreamjob.model;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Post {
+public class Post implements Serializable {
     private int id;
     private String name;
+    private boolean visible;
 
     public Post(int id, String name) {
         this.id = id;
@@ -25,6 +27,14 @@ public class Post {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override
