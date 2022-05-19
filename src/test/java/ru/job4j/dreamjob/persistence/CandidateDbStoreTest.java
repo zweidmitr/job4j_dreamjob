3b1b78d501b2c4fc4a4b2b0ca54ecb8e604d37aa
zweidmitr <zweidmitr@gmail.com>
2022-05-19 21:21:25 +0300

    3. Тестирование базы данных. Liquibase H2

--- src/test/java/ru/job4j/dreamjob/persistence/CandidateDbStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/persistence/CandidateDbStoreTest.java
@@ -4,19 +4,32 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Test;
 import ru.job4j.dreamjob.Main;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.model.Post;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class CandidateDbStoreTest {
-    private BasicDataSource pool = new Main().loadPool();
-    private CandidateDbStore store = new CandidateDbStore(pool);
-
-    private Candidate candidate = new Candidate(0, "name", "description");
 
     @Test
-    public void whenCreateCandidate() {
+    public void whenFindALlCandidate() {
+        BasicDataSource pool = new Main().loadPool();
+        CandidateDbStore store = new CandidateDbStore(pool);
+        Candidate oneCandidate = new Candidate(1, "one", "descOne");
+        Candidate twoCandidate = new Candidate(2, "two", "descTwo");
+        Candidate threeCandidate = new Candidate(3, "three", "descThree");
+
+        List<Candidate> result = List.of(oneCandidate, twoCandidate, threeCandidate);
+        assertThat(result, is(store.findAll()));
+    }
+
+    @Test
+    public void whenAddCandidateAndFindById() {
+        BasicDataSource pool = new Main().loadPool();
+        CandidateDbStore store = new CandidateDbStore(pool);
+        Candidate candidate = new Candidate(0, "name", "description");
+
         store.add(candidate);
         Candidate candidateDb = store.findById(candidate.getId());
         assertThat(candidateDb.getName(), is(candidate.getName()));
@@ -24,6 +37,10 @@ public class CandidateDbStoreTest {
 
     @Test
     public void whenUpdateCandidate() {
+        BasicDataSource pool = new Main().loadPool();
+        CandidateDbStore store = new CandidateDbStore(pool);
+        Candidate candidate = new Candidate(0, "name", "description");
+
         store.add(candidate);
         candidate.setName("new candidate name");
         store.update(candidate);
--- src/test/java/ru/job4j/dreamjob/persistence/PostDbStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/persistence/PostDbStoreTest.java
@@ -7,18 +7,41 @@ import ru.job4j.dreamjob.Main;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.services.CityService;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class PostDbStoreTest {
 
-    private CityService cityService = new CityService(new CityStore());
-    private BasicDataSource pool = new Main().loadPool();
-    private PostDbStore store = new PostDbStore(pool, cityService);
-    private Post post = new Post(0, "name");
+    @Test
+    public void whenFindAll() {
+        CityService cityService = new CityService(new CityStore());
+        BasicDataSource pool = new Main().loadPool();
+        PostDbStore store = new PostDbStore(pool, cityService);
+        Post onePost = new Post(1, "one");
+        Post twoPost = new Post(2, "two");
+        Post threePost = new Post(3, "three");
+
+        onePost.setCity(cityService.findById(1));
+        twoPost.setCity(cityService.findById(2));
+        threePost.setCity(cityService.findById(3));
+
+        store.add(onePost);
+        store.add(twoPost);
+        store.add(threePost);
+
+        List<Post> results = List.of(onePost, twoPost, threePost);
+        assertThat(results, is(store.findAll()));
+
+    }
 
     @Test
-    public void whenCreatePost() {
+    public void whenAddPostAndFindById() {
+        var cityService = new CityService(new CityStore());
+        PostDbStore store = new PostDbStore(new Main().loadPool(), cityService);
+        Post post = new Post(0, "Java job");
+
         post.setCity(cityService.findById(1));
         store.add(post);
         Post postInDb = store.findById(post.getId());
@@ -27,6 +50,11 @@ public class PostDbStoreTest {
 
     @Test
     public void whenUpdatePost() {
+        CityService cityService = new CityService(new CityStore());
+        BasicDataSource pool = new Main().loadPool();
+        PostDbStore store = new PostDbStore(pool, cityService);
+        Post post = new Post(0, "name");
+
         store.add(post);
         post.setName("new post name");
         store.update(post);
